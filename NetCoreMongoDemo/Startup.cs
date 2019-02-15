3155b0ae5ed6daf5901ca4f8b8559b50e2a27c49
ChunJie.Wang <734463238@qq.com>
2019-02-15 16:38:34 +0800

    feat:增加swagger和版本管理

--- NetCoreMongoDemo/Startup.cs
+++ NetCoreMongoDemo/Startup.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Microsoft.Extensions.PlatformAbstractions;
 using NetCoreMongoDemo.Services;
+using Swashbuckle.AspNetCore.Swagger;
+using System.IO;
+using System.Reflection;
 
 namespace NetCoreMongoDemo
 {
@@ -22,22 +21,109 @@ namespace NetCoreMongoDemo
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<BookService>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            //版本控制
+            services.AddMvcCore();
+            services.AddVersionedApiExplorer(o => o.GroupNameFormat = "'v'VVV");
+            services.AddApiVersioning(option =>
+            {
+                // allow a client to call you without specifying an api version
+                // since we haven't configured it otherwise, the assumed api version will be 1.0
+                option.AssumeDefaultVersionWhenUnspecified = true;
+                option.ReportApiVersions = false;
+            });
+
+            services.AddSwaggerGen(
+                options =>
+                {
+                    // resolve the IApiVersionDescriptionProvider service
+                    // note: that we have to build a temporary service provider here because one has not been created yet
+                    var provider = services.BuildServiceProvider().GetRequiredService<IApiVersionDescriptionProvider>();
+
+                    // add a swagger document for each discovered API version
+                    // note: you might choose to skip or document deprecated API versions differently
+                    foreach (var description in provider.ApiVersionDescriptions)
+                    {
+                        options.SwaggerDoc(description.GroupName, CreateInfoForApiVersion(description));
+                    }
+
+                    // add a custom operation filter which sets default values
+                    options.OperationFilter<SwaggerDefaultValues>();
+
+                    //
+                    // integrate xml comments
+                    options.IncludeXmlComments(XmlCommentsFilePath);
+                    //options.IncludeXmlComments(XmlModelsFilePath);
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApiVersionDescriptionProvider provider)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
+            app.UseHttpsRedirection();
             app.UseMvc();
+
+            //Swagger
+            app.UseSwagger();
+            app.UseSwaggerUI(
+                options =>
+                {
+                    foreach (var description in provider.ApiVersionDescriptions)
+                    {
+                        options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
+                    }
+                });
+        }
+
+        static string XmlCommentsFilePath
+        {
+            get
+            {
+                var basePath = PlatformServices.Default.Application.ApplicationBasePath;
+                var fileName = typeof(Startup).GetTypeInfo().Assembly.GetName().Name + ".xml";
+                return Path.Combine(basePath, fileName);
+            }
+        }
+
+        //static string XmlModelsFilePath
+        //{
+        //    get
+        //    {
+        //        var basePath = PlatformServices.Default.Application.ApplicationBasePath;
+        //        var fileName = typeof(UserInfoModel).GetTypeInfo().Assembly.GetName().Name + ".xml";
+        //        return Path.Combine(basePath, fileName);
+        //    }
+        //}
+
+        static Info CreateInfoForApiVersion(ApiVersionDescription description)
+        {
+            var info = new Info()
+            {
+                Title = $"NetCoreMongoDemo v{description.ApiVersion}",
+                Version = description.ApiVersion.ToString(),
+                Description = "版本（右上角切换）<br/>",
+                Contact = new Contact() { Name = "如有问题请联系我，734463238@qq.com" }
+            };
+
+            if (description.IsDeprecated)
+            {
+                info.Description += "<br/><b>南山</b>";
+            }
+
+            return info;
         }
     }
 }
